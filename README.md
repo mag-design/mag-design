48f42903399cc5801956036f69cca79523b333a1
Yao Chang <yaochang@baidu.com>
2017-09-21 17:51:31 +0800

    change build process

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Mag Design
+
+## Usage
+### link
+```html
+<link rel="stylesheet" href="mag-design/dist/mag-design.min.css">
+```
+### Importing Styles
+#### Importing Compiled CSS
+```javascript
+// import all styles
+import "mag-design/dist/mag-design.css";
+
+// import one style like color
+import "mag-design/dist/css/color.css";
+```
+#### Importing Precompiled Less
+```less
+// import all styles
+@import "mag-design/dist/mag-design.less";
+
+// import one style like color
+@import "mag-design/dist/css/color.less";
+```
--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,41 @@ var rename = require("gulp-rename");
 var autoprefix = new LessAutoprefix();
 
 var paths = [
-    'src/css/index.less'
+    'src/css/index.less',
+    'src/css/category/*'
 ];
+
+var lessOptions = function () {
+    return less({
+        plugins: [autoprefix]
+    });
+};
+
+var cleanCSSOptions = function () {
+    return cleanCSS({
+        format: {
+            breaks: {
+                afterAtRule: true,
+                afterBlockBegins: true,
+                afterBlockEnds: true,
+                afterComment: true,
+                afterProperty: true,
+                afterRuleBegins: true,
+                afterRuleEnds: true,
+                beforeBlockEnds: true,
+                betweenSelectors: true
+            },
+            indentBy: 4,
+            indentWith: 'space',
+            spaces: {
+                aroundSelectorRelation: true,
+                beforeBlockBegins: true,
+                beforeValue: true
+            },
+            wrapAt: false
+        }
+    });
+};
 
 gulp.task('clean', function () {
     return gulp.src('dist/*', {read: false})
@@ -18,49 +51,35 @@ gulp.task('clean', function () {
 });
 
 gulp.task('build', ['clean'], function () {
-    return gulp.src(paths)
+    return gulp.src(paths[0])
         .pipe(sourcemaps.init())
-        .pipe(less({
-            plugins: [autoprefix]
-        }))
-        .pipe(cleanCSS({
-            format: {
-                breaks: {
-                    afterAtRule: true,
-                    afterBlockBegins: true,
-                    afterBlockEnds: true,
-                    afterComment: true,
-                    afterProperty: true,
-                    afterRuleBegins: true,
-                    afterRuleEnds: true,
-                    beforeBlockEnds: true,
-                    betweenSelectors: true
-                  },
-                  indentBy: 4,
-                  indentWith: 'space',
-                  spaces: {
-                    aroundSelectorRelation: true,
-                    beforeBlockBegins: true,
-                    beforeValue: true
-                  },
-                  wrapAt: false
-            }
-        }))
+        .pipe(lessOptions())
+        .pipe(cleanCSSOptions())
         .pipe(rename('mag-design.css'))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build-min', ['clean'], function () {
-    return gulp.src(paths)
+    return gulp.src(paths[0])
         .pipe(sourcemaps.init())
-        .pipe(less({
-            plugins: [autoprefix]
-        }))
+        .pipe(lessOptions())
         .pipe(cleanCSS())
         .pipe(rename('mag-design.min.css'))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['build', 'build-min']);
+gulp.task('build-module-css', ['clean'], function () {
+    return gulp.src(paths[1])
+        .pipe(lessOptions())
+        .pipe(cleanCSSOptions())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('build-module-less', ['clean'], function () {
+    return gulp.src(paths[1])
+        .pipe(gulp.dest('dist/less'));
+});
+
+gulp.task('default', ['build', 'build-min', 'build-module-less', 'build-module-css']);
